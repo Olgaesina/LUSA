34b6b0a4579b0c0aee6fd9bbe2c719309ce3226d
annal <alexesin1979@yandex.ru>
2025-07-10 21:45:36 +0300

    Строки

---
+++ задачи 09.07.py
@@ -0,0 +1,11 @@
+from pyexpat.errors import messages
+
+string1 = "Привет,"
+string2 = "мир!"
+result = string1 + string2
+print(result)
+
+name = "Анна"
+age = 25
+messages = f"{name},{age} лет"
+print(messages)
