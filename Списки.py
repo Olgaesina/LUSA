2a0de154fa921d672f3b2ad3a577bfb1f0329bee
annal <alexesin1979@yandex.ru>
2025-07-17 19:08:48 +0300

    Списки 3 урок

---
+++ Списки.py
@@ -0,0 +1,18 @@
+colors = ["красный", "зеленый", "синий"]
+print(colors)
+
+animals = ["кошка", "собака", "хомяк", "попугай"]
+del animals [0:3]
+print(animals)
+
+animals = ["кошка", "собака", "хомяк", "попугай"]
+third_animals = animals[2]
+print(third_animals)
+
+fruits = ["яблоко", "груша", "слива"]
+fruits.insert(2, "апельсин")
+print(fruits)
+
+cars = ["BMW", "Toyota", "Audi", "Kia"]
+cars [2] = "Mercedes"
+print(cars)
